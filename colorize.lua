0ec7c64c0a2a2cd289b52b82c4a28386e0202381
unanimated <infernal.eternal@safe-mail.net>
2014-07-06 13:45:56 +0200

    3.6

--- colorize.lua
+++ colorize.lua
@@ -1,7 +1,7 @@
 ﻿script_name="Colorize"
 script_description="Does things with colours"
 script_author="unanimated"
-script_version="3.52"
+script_version="3.6"
 
 --[[
 
@@ -321,19 +321,6 @@ function shift(subs,sel)
 end
 
 function matchcolors(subs,sel)
-match_config=
-	{
-	{x=0,y=0,width=1,height=1,class="label",label="copy primary\nto outline"},
-	{x=2,y=0,width=1,height=1,class="label",label="copy outline\nto primary"},
-	{x=4,y=0,width=1,height=1,class="label",label="copy primary\nto shadow"},
-	{x=6,y=0,width=1,height=1,class="label",label="copy outline\nto shadow"},
-	{x=8,y=0,width=1,height=1,class="label",label="switch primary\nand outline"},
-	{x=10,y=0,width=1,height=1,class="label",label="invert all \ncolours"},
-	}
-if pressed=="Match Colours" then
-	press,rez=aegisub.dialog.display(match_config,{"c --> 3c","3c --> c","c --> 4c","3c --> 4c","c <--> 3c","Invert","Cancel"},{close='Cancel'})
-end
-
     for x, i in ipairs(sel) do
         aegisub.progress.title(string.format("Colorizing line %d/%d",x,#sel))
 	local line=subs[i]
@@ -365,7 +352,7 @@ end
 	    if not text:match("^{\\") then text=text:gsub("^","{\\}") end
 
 	-- 1-->3   match outline to primary
-	if pressed=="Match Colours" and press=="c --> 3c" then
+	if pressed=="Match Colours" and res.match13 then
 	    for ctags in text:gmatch("({\\[^}]-})") do
 		ctags2=nil
 		if ctags:match("\\3c") and not ctags:match("\\1?c") then ctags2=ctags:gsub("\\3c&H%w+&","\\3c"..primary) end
@@ -380,7 +367,7 @@ end
 	end
 
 	-- 3-->1   match primary to outline
-	if pressed=="Match Colours" and press=="3c --> c" then
+	if pressed=="Match Colours" and res.match31 then
 	    for ctags in text:gmatch("({\\[^}]-})") do
 		ctags2=nil
 		if ctags:match("\\1?c") and not ctags:match("\\3c") then ctags2=ctags:gsub("\\1?c&H%w+&","\\c"..outline) end
@@ -394,23 +381,8 @@ end
 	    end
 	end
 	
-	-- 3-->4   match shadow to outline
-	if pressed=="Match Colours" and press=="3c --> 4c" then
-	    for ctags in text:gmatch("({\\[^}]-})") do
-		ctags2=nil
-		if ctags:match("\\4c") and not ctags:match("\\3c") then ctags2=ctags:gsub("\\4c&H%w+&","\\4c"..outline) end
-		if ctags:match("\\4c") and ctags:match("\\3c") then 
-		  tempc=ctags:match("\\3c(&H%w+&)") ctags2=ctags:gsub("\\4c&H%w+&","\\4c"..tempc) end
-		if ctags:match("\\3c") and not ctags:match("\\4c") then
-		  ctags2=ctags:gsub("\\3c(&H%w+&)","\\3c%1\\4c%1") end
-		if ctags==text:match("^({\\[^}]-})") and not ctags:match("\\4c") and not ctags:match("\\3c") then
-		  ctags2=ctags:gsub("^({\\[^}]-)}","%1\\4c"..outline.."}") end
-		if ctags2~=nil then ctags=esc(ctags) text=text:gsub(ctags,ctags2) end
-	    end
-	end
-	
-	-- 3-->4   match shadow to primary
-	if pressed=="Match Colours" and press=="c --> 4c" then
+	-- 1-->4   match shadow to primary
+	if pressed=="Match Colours" and res.match14 then
 	    for ctags in text:gmatch("({\\[^}]-})") do
 		ctags2=nil
 		if ctags:match("\\4c") and not ctags:match("\\c") then ctags2=ctags:gsub("\\4c&H%w+&","\\4c"..primary) end
@@ -423,9 +395,24 @@ end
 		if ctags2~=nil then ctags=esc(ctags) text=text:gsub(ctags,ctags2) end
 	    end
 	end
+	
+	-- 3-->4   match shadow to outline
+	if pressed=="Match Colours" and res.match34 then
+	    for ctags in text:gmatch("({\\[^}]-})") do
+		ctags2=nil
+		if ctags:match("\\4c") and not ctags:match("\\3c") then ctags2=ctags:gsub("\\4c&H%w+&","\\4c"..outline) end
+		if ctags:match("\\4c") and ctags:match("\\3c") then 
+		  tempc=ctags:match("\\3c(&H%w+&)") ctags2=ctags:gsub("\\4c&H%w+&","\\4c"..tempc) end
+		if ctags:match("\\3c") and not ctags:match("\\4c") then
+		  ctags2=ctags:gsub("\\3c(&H%w+&)","\\3c%1\\4c%1") end
+		if ctags==text:match("^({\\[^}]-})") and not ctags:match("\\4c") and not ctags:match("\\3c") then
+		  ctags2=ctags:gsub("^({\\[^}]-)}","%1\\4c"..outline.."}") end
+		if ctags2~=nil then ctags=esc(ctags) text=text:gsub(ctags,ctags2) end
+	    end
+	end
 
 	-- 1<-->3   switch primary and border
-	if pressed=="Match Colours" and press=="c <--> 3c" then
+	if pressed=="Match Colours" and res.match131 then
 	    if text:match("^{\\") then
 		tags=text:match("^({\\[^}]-})")
 		if tags:match("\\1?c") then tags=tags:gsub("\\1?c","\\tempc")
@@ -449,7 +436,7 @@ end
 	end
 
 	-- Invert All Colours  
-	if pressed=="Match Colours" and press=="Invert" then
+	if pressed=="Match Colours" and res.invert then
 	    if not text:match("^{\\") then text="{\\what}"..text end
 		tags=text:match("^({\\[^}]-})")
 		for n=1,4 do
@@ -603,6 +590,12 @@ function repetition()
 	res.mktag=lastmktag
 	res.gclrs=lastgclrs
 	res.gcl=lastgcl
+	res.match13=lastmatch13
+	res.match31=lastmatch31
+	res.match14=lastmatch14
+	res.match34=lastmatch34
+	res.match131=lastmatch131
+	res.invert=lastinvert
 	end
 end
 
@@ -610,56 +603,64 @@ function colorize(subs,sel)
 	dialog_config=
 	{
 	{x=0,y=0,width=1,height=1,class="label",label="Colours"},
-	{x=1,y=0,width=1,height=1,class="dropdown",name="clrs",items={"2","3","4","5"},value="2"},
+	{x=1,y=0,width=2,height=1,class="dropdown",name="clrs",items={"2","3","4","5"},value="2"},
 	
 	{x=0,y=1,width=1,height=1,class="label",label="Shift base:"},
-	{x=1,y=1,width=1,height=1,class="dropdown",name="shit",items={"# of colours","line"},value="# of colours"},
+	{x=1,y=1,width=2,height=1,class="dropdown",name="shit",items={"# of colours","line"},value="# of colours"},
 	
 	{x=0,y=2,width=1,height=1,class="label",label="Apply to:  "},
-	{x=1,y=2,width=1,height=1,class="dropdown",name="kol",items={"primary","border","shadow","secondary"},value="primary"},
+	{x=1,y=2,width=2,height=1,class="dropdown",name="kol",items={"primary","border","shadow","secondary"},value="primary"},
 	    
-	{x=3,y=0,width=1,height=1,class="label",label="  1 "},
-	{x=3,y=1,width=1,height=1,class="label",label="  2 "},
-	{x=3,y=2,width=1,height=1,class="label",label="  3 "},
-	{x=3,y=3,width=1,height=1,class="label",label="  4 "},
-	{x=3,y=4,width=1,height=1,class="label",label="  5 "},
+	{x=4,y=0,width=1,height=1,class="label",label="  1 "},
+	{x=4,y=1,width=1,height=1,class="label",label="  2 "},
+	{x=4,y=2,width=1,height=1,class="label",label="  3 "},
+	{x=4,y=3,width=1,height=1,class="label",label="  4 "},
+	{x=4,y=4,width=1,height=1,class="label",label="  5 "},
 	
-	{x=4,y=0,width=1,height=1,class="color",name="c1" },
-	{x=4,y=1,width=1,height=1,class="color",name="c2" },
-	{x=4,y=2,width=1,height=1,class="color",name="c3" },
-	{x=4,y=3,width=1,height=1,class="color",name="c4" },
-	{x=4,y=4,width=1,height=1,class="color",name="c5" },
+	{x=5,y=0,width=1,height=1,class="color",name="c1" },
+	{x=5,y=1,width=1,height=1,class="color",name="c2" },
+	{x=5,y=2,width=1,height=1,class="color",name="c3" },
+	{x=5,y=3,width=1,height=1,class="color",name="c4" },
+	{x=5,y=4,width=1,height=1,class="color",name="c5" },
 	
-	{x=0,y=3,width=2,height=1,class="checkbox",name="join",label="Don't join with other tags",value=false },
-	{x=0,y=4,width=3,height=1,class="checkbox",name="cont",label="Continuous shift line by line",value=false },
-	{x=0,y=5,width=3,height=1,class="checkbox",name="word",label="Colorize by word",value=false },
-	{x=0,y=6,width=4,height=1,class="checkbox",name="gcl",label="Set colours across whole line:",value=false },
-	{x=4,y=6,width=1,height=1,class="dropdown",name="gclrs",items={"2","3","4","5","6","7","8","9","10"},value="3"},
-	{x=0,y=7,width=5,height=1,class="checkbox",name="rept",label="repeat with last settings (any function)",value=false },
+	{x=0,y=3,width=3,height=1,class="checkbox",name="join",label="Don't join with other tags",value=false },
+	{x=0,y=4,width=4,height=1,class="checkbox",name="cont",label="Continuous shift line by line",value=false },
+	{x=0,y=5,width=4,height=1,class="checkbox",name="word",label="Colorize by word",value=false },
+	{x=0,y=6,width=5,height=1,class="checkbox",name="gcl",label="Set colours across whole line:",value=false },
+	{x=5,y=6,width=1,height=1,class="dropdown",name="gclrs",items={"2","3","4","5","6","7","8","9","10"},value="3"},
+	{x=0,y=7,width=6,height=1,class="checkbox",name="rept",label="repeat with last settings (any function)",value=false },
 		
-	{x=5,y=0,width=1,height=1,class="label",label=" "},
+	{x=6,y=0,width=1,height=1,class="label",label=" "},
 		
-	{x=6,y=0,width=1,height=1,class="label",label="Red: "},
-	{x=7,y=0,width=2,height=1,class="intedit",name="R",value=0,min=-255,max=255},
-	{x=6,y=1,width=1,height=1,class="label",label="Green: "},
-	{x=7,y=1,width=2,height=1,class="intedit",name="G",value=0,min=-255,max=255},
-	{x=6,y=2,width=1,height=1,class="label",label="Blue: "},
-	{x=7,y=2,width=2,height=1,class="intedit",name="B",value=0,min=-255,max=255},
-	{x=6,y=4,width=1,height=1,class="label",label="Brightness:"},
-	{x=7,y=4,width=2,height=1,class="intedit",name="bright",value=0,min=-255,max=255},
-	{x=6,y=3,width=5,height=1,class="label",label="You can use values from -255 to 255."},
-	{x=6,y=5,width=5,height=1,class="label",label="1 step for brightness equals 1 step for R+G+B."},
+	{x=7,y=0,width=1,height=1,class="label",label="Red: "},
+	{x=8,y=0,width=3,height=1,class="intedit",name="R",value=0,min=-255,max=255},
+	{x=7,y=1,width=1,height=1,class="label",label="Green: "},
+	{x=8,y=1,width=3,height=1,class="intedit",name="G",value=0,min=-255,max=255},
+	{x=7,y=2,width=1,height=1,class="label",label="Blue: "},
+	{x=8,y=2,width=3,height=1,class="intedit",name="B",value=0,min=-255,max=255},
+	{x=7,y=4,width=1,height=1,class="label",label="Brightness:"},
+	{x=8,y=4,width=3,height=1,class="intedit",name="bright",value=0,min=-255,max=255},
+	{x=7,y=3,width=5,height=1,class="label",label="You can use values from -255 to 255."},
+	{x=7,y=5,width=5,height=1,class="label",label="1 step for brightness equals 1 step for R+G+B."},
 
-	{x=6,y=6,width=1,height=1,class="checkbox",name="k1",label="\\c       ", value=true  },
-	{x=7,y=6,width=1,height=1,class="checkbox",name="k3",label="\\3c      ",value=false },
-	{x=8,y=6,width=1,height=1,class="checkbox",name="k4",label="\\4c      ",value=false },
-	{x=9,y=6,width=1,height=1,class="checkbox",name="k2",label="\\2c",value=false },
-	{x=6,y=7,width=5,height=1,class="checkbox",name="mktag",label="apply even to colours without tags in line", value=false },
+	{x=7,y=6,width=1,height=1,class="checkbox",name="k1",label="\\c       ",value=true  },
+	{x=8,y=6,width=1,height=1,class="checkbox",name="k3",label="\\3c      ",value=false },
+	{x=9,y=6,width=1,height=1,class="checkbox",name="k4",label="\\4c      ",value=false },
+	{x=10,y=6,width=1,height=1,class="checkbox",name="k2",label="\\2c",value=false },
+	{x=7,y=7,width=5,height=1,class="checkbox",name="mktag",label="apply even to colours without tags in line",value=false },
 	
-	{x=9,y=0,width=1,height=1,class="label",label="[v "..script_version.."]"},
+	{x=0,y=8,width=1,height=1,class="label",label="Match col.:"},
+	{x=1,y=8,width=1,height=1,class="checkbox",name="match13",label="c->3c  ",value=false,hint="copy primary to outline"},
+	{x=2,y=8,width=3,height=1,class="checkbox",name="match31",label="3c->c",value=false,hint="copy outline to primary"},
+	{x=5,y=8,width=1,height=1,class="checkbox",name="match14",label="c->4c",value=false,hint="copy primary to shadow"},
+	{x=7,y=8,width=1,height=1,class="checkbox",name="match34",label="3c->4c",value=false,hint="copy outline to shadow"},
+	{x=8,y=8,width=1,height=1,class="checkbox",name="match131",label="c<->3c",value=false,hint="switch primary and outline"},
+	{x=9,y=8,width=1,height=1,class="checkbox",name="invert",label="invert",value=false,hint="invert colours"},
 	
-	} 	
-	pressed, res=aegisub.dialog.display(dialog_config,{"Colorize","Shift","Match Colours","RGB","Brightness","Cancel"},{ok='Colorize',close='Cancel'})
+	{x=10,y=8,width=2,height=1,class="label",label="[ver. "..script_version.."]"},
+	
+	}
+	pressed,res=aegisub.dialog.display(dialog_config,{"Colorize","Shift","Match Colours","RGB","Brightness","Cancel"},{ok='Colorize',close='Cancel'})
 	if pressed=="Cancel" then aegisub.cancel() end
 	if pressed=="Colorize" then repetition() 
 	    if res.gcl then gcolors(subs,sel) else colors(subs,sel) end
@@ -667,28 +668,20 @@ function colorize(subs,sel)
 	if pressed=="Shift" then repetition() shift(subs,sel) end
 	if pressed=="Match Colours" or pressed=="RGB" or pressed=="Brightness" then repetition() styleget(subs) matchcolors(subs,sel) end
 	
-	lastclrs=res.clrs
-	lastshit=res.shit
-	lastkol=res.kol
-	lastc1=res.c1
-	lastc2=res.c2
-	lastc3=res.c3
-	lastc4=res.c4
-	lastc5=res.c5
-	lastjoin=res.join
-	lastcont=res.cont
-	lastword=res.word
-	lastR=res.R
-	lastG=res.G
-	lastB=res.B
-	lastbright=res.bright
-	lastk1=res.k1
-	lastk2=res.k2
-	lastk3=res.k3
-	lastk4=res.k4
-	lastmktag=res.mktag
-	lastgcl=res.gcl
-	lastgclrs=res.gclrs
+	lastclrs=res.clrs		lastshit=res.shit
+	lastkol=res.kol			lastc1=res.c1
+	lastc2=res.c2			lastc3=res.c3
+	lastc4=res.c4			lastc5=res.c5
+	lastjoin=res.join		lastcont=res.cont
+	lastword=res.word		lastR=res.R
+	lastG=res.G			lastB=res.B
+	lastbright=res.bright		lastmktag=res.mktag
+	lastk1=res.k1			lastk2=res.k2
+	lastk3=res.k3			lastk4=res.k4
+	lastgcl=res.gcl			lastgclrs=res.gclrs
+	lastmatch13=res.match13		lastmatch31=res.match31
+	lastmatch14=res.match14		lastmatch34=res.match34
+	lastmatch131=res.match131	lastinvert=res.invert
     
 	aegisub.set_undo_point(script_name)
 	return sel
