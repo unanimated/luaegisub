f917a7d524bbdd15b4c69e211bff452ea8b98455
unanimated <infernal.eternal@safe-mail.net>
2014-08-25 17:54:35 +0200

    2.2

--- nnn.lua
+++ nnn.lua
@@ -1,4 +1,4 @@
--- First function inserts a linebreak where it seems logical - after periods, commas, before "and," etc. If the line has one, it removes it.
+﻿-- First function inserts a linebreak where it seems logical - after periods, commas, before "and," etc. If the line has one, it removes it.
 --   This will not always produce the desired result, but mostly it works fairly well.
 --   There's a limit for how unevenly a line can be split, so if it's over the ratio, it nukes the \N and goes to the next step.
 --   If the stuff above didn't work out, a linebreak is inserted in the middle of the line if restrictions in settings allow it.
@@ -8,49 +8,104 @@
 --   When it reaches the last word, it removes the break.
 -- Third function shifts linebreaks back and is disabled by default.
 -- You can bind each function to a different hotkey and combine them as needed.
+-- To bring up Setup, type 'setup' into the effect field.
 
 script_name="Line Breaker"
 script_description="insert/shift linebreaks"
 script_author="unanimated"
-script_version="2.1"
-
---	SETTINGS	--	-- options are either numbers or true/false							[default value]
-
-min_characters=0		-- minimum characters needed to insert a linebreak (ie no breaks in lines under X characters)	[42]
-min_words=3			-- minimum words needed to insert a linebreak							[3]
-put_break_in_the_middle=true	-- put a linebreak in the middle with the insert function if all else fails			[true]
-middle_min_characters=40	-- minimum of characters needed to insert a linebreak in the middle				[45]
-force_breaks_in_middle=false	-- force breaks in the middle of the line rather than after commas etc. (not recommended because stupid)
-enable_shift_backwards=true	-- enables shifting backwards (adds a new item to the automation menu)				[false]
-disable_dialog=false		-- if no break is placed with the insert function, a dialog lets you choose it manually.	[false]
-allow_break_between_two=true	-- allow a break if there are only 2 words. good for TS, not much for editing (overrides min_ settings)	[false]
-do_balance_checks=true		-- enable checking the ratio between top and bottom line (only for lines with over 4 words)	[true]
-max_ratio=2.2			-- maximum ratio between top and bottom line allowed (if previous setting enabled)		[2.2]
-do_not_break_1liners=false	-- won't break lines that don't break naturally. disables manual breaking and break between 2.	[false]
-
---	--	--	--
+script_version="2.2"
 
 re=require'aegisub.re'
 
-function esc(str)
-str=str:gsub("%%","%%%%")
-str=str:gsub("%(","%%%(")
-str=str:gsub("%)","%%%)")
-str=str:gsub("%[","%%%[")
-str=str:gsub("%]","%%%]")
-str=str:gsub("%.","%%%.")
-str=str:gsub("%*","%%%*")
-str=str:gsub("%-","%%%-")
-str=str:gsub("%+","%%%+")
-str=str:gsub("%?","%%%?")
-return str
+function setupcheck()
+file=io.open(breaksetup)
+    if file==nil then setup() end
+end
+
+  nnngui={
+  {x=0,y=0,width=2,class="label",label="---------------- Line Breaker Setup ----------------"},
+  {x=0,y=1,width=1,class="label",label="min. characters:"},
+  {x=1,y=1,width=1,class="intedit",name="minchar",value=0},
+  {x=0,y=2,width=1,class="label",label="min. words:"},
+  {x=1,y=2,width=1,class="intedit",name="minword",value=3},
+  {x=0,y=3,width=2,class="checkbox",name="middle",label="linebreak in the middle if all else fails",value=true},
+  {x=0,y=4,width=1,class="label",label="^ min. characters:"},
+  {x=1,y=4,width=1,class="intedit",name="midminchar",value=0},
+  {x=0,y=5,width=3,class="checkbox",name="forcemiddle",label="force breaks in the middle",hint="rather than after commas etc."},
+  {x=0,y=6,width=3,class="checkbox",name="disabledialog",label="disable dialog for making manual breaks"},
+  {x=0,y=7,width=3,class="checkbox",name="allowtwo",label="allow a break if there are only two words",value=true},
+  {x=0,y=8,width=3,class="checkbox",name="balance",label="enable balance checks",value=true,hint="check ratio between top and bottom line"},
+  {x=0,y=9,width=1,class="label",label="^ max. ratio:"},
+  {x=1,y=9,width=1,class="intedit",name="maxratio",value=2.2},
+  {x=0,y=10,width=3,class="checkbox",name="nobreak1",label="don't break 1-liners",hint="disables manual breaking && break between 2"},
+  }
+
+function setup()
+file=io.open(breaksetup)
+  if file~=nil then
+    konf=file:read("*all")
+    io.close(file)
+	for key,val in ipairs(nnngui) do
+	  if val.class=="checkbox" or val.class=="intedit" then
+	    if konf:match(val.name) then val.value=detf(konf:match(val.name..":(.-)\n")) end
+	  end
+	end
+  end
+  P,res=ADD(nnngui,{"Save","Cancel"},{ok='Save',close='Cancel'})
+  if P=="Cancel" then ak() end
+  nnncf="Line Breaker Settings\n\n"
+  for key,val in ipairs(nnngui) do
+    if val.class=="checkbox" then nnncf=nnncf..val.name..":"..tf(res[val.name]).."\n" end
+    if val.class=="intedit" then nnncf=nnncf..val.name..":"..res[val.name].."\n" end
+  end
+  
+  file=io.open(breaksetup,"w")
+  file:write(nnncf)
+  file:close()
+  ADD({{class="label",label="Config Saved to:\n"..breaksetup.."\nTo bring up the setup again, type 'setup' into 'effect'."}},{"OK"},{close='OK'})
+  ak()
+end
+
+function tf(val)
+    if val==true then ret="true" else ret="false" end
+    return ret
+end
+
+function detf(txt)
+    if txt=="true" then ret=true
+    elseif txt=="false" then ret=false
+    else ret=tonumber(txt) end
+    return ret
+end
+
+function readconfig()
+file=io.open(breaksetup)
+    if file~=nil then
+	konf=file:read("*all")
+	io.close(file)
+	min_characters=detf(konf:match("minchar:(.-)\n"))
+	min_words=detf(konf:match("minword:(.-)\n"))
+	put_break_in_the_middle=detf(konf:match("middle:(.-)\n"))
+	middle_min_char=detf(konf:match("midminchar:(.-)\n"))
+	force_middle=detf(konf:match("forcemiddle:(.-)\n"))
+	disable_dialog=detf(konf:match("disabledialog:(.-)\n"))
+	allow_two=detf(konf:match("allowtwo:(.-)\n"))
+	balance_checks=detf(konf:match("balance:(.-)\n"))
+	max_ratio=detf(konf:match("maxratio:(.-)\n"))
+	do_not_break_1liners=detf(konf:match("nobreak1:(.-)\n"))
+    end
 end
 
 function nnn(subs, sel)
+  ADD=aegisub.dialog.display
+  ADP=aegisub.decode_path
+  ak=aegisub.cancel
+  breaksetup=ADP("?user").."\\lbreak.conf"
+  setupcheck()
+  readconfig()
   for i=1, #subs do
     if subs[i].class=="style" then
       local st=subs[i]
-      if stylename==st.name then styleref=st end
       if st.name=="Default" then defaref=st defleft=st.margin_l defright=st.margin_r end
     end
     if subs[i].class=="info" then
@@ -63,9 +118,11 @@ function nnn(subs, sel)
   end
 
   for x, i in ipairs(sel) do
-    aegisub.progress.title(string.format("Processing line: %d/%d",x,#sel))
+    if aegisub.progress.is_cancelled() then aegisub.cancel() end
     line=subs[i]
     text=line.text
+    if line.effect=="setup" then setup() end
+    aegisub.progress.title(string.format("Processing line: %d/%d",x,#sel))
 	
     if line.style=="Default" then styleref=defaref else styleref=stylechk(subs,line.style) end
 
@@ -98,8 +155,6 @@ function nnn(subs, sel)
 
 	-- count words
 	wrd=0	for word in nocom:gmatch("[^%s]+") do wrd=wrd+1 end
-
-	--aegisub.log("\n text - "..tekst)
 
 	    -- put breaks after . , ? ! that are not at the end
 	    tekst=tekst:gsub("([^%.])%.%s","%1. \\N")
@@ -138,8 +193,8 @@ function nnn(subs, sel)
 	    tekst=words(words3)
 
 	    -- insert break in the middle of the line
-	    if force_breaks_in_middle then tekst=db(tekst) end
-	    if put_break_in_the_middle and nocomlength>=middle_min_characters and not tekst:match("\\N") then
+	    if force_middle then tekst=db(tekst) end
+	    if put_break_in_the_middle and nocomlength>=middle_min_char and not tekst:match("\\N") then
 		tekst="\\N"..tekst
 		diff=250	stop=0
 		while stop==0 do
@@ -165,7 +220,6 @@ function nnn(subs, sel)
 	    :gsub(" lot(s?) \\Nof "," lot%1 of \\N")
 	    :gsub(" \\Nme "," me \\N")
 	    :gsub("^ ","")
-	    --aegisub.log("\n tekst "..tekst)
 	    tekstb=balance(tekst)
 	    if tekstb~=tekst then tekst=backup2 end
 	    
@@ -195,7 +249,7 @@ function nnn(subs, sel)
 	    if nocomlength<min_characters or wrd<min_words then tekst=db(tekst) end
 
 	    -- break if there are only 2 words in the line
-	    if wrd==2 and allow_break_between_two then tekst=tekst:gsub("(%w+%p?)%s(%w+%p?)%s?","%1 \\N%2") end
+	    if wrd==2 and allow_two then tekst=tekst:gsub("(%w+%p?)%s(%w+%p?)%s?","%1 \\N%2") end
 	    
 	    -- don't break 1-liners if in settings
 	    if do_not_break_1liners and vidth>=width then tekst=db(tekst) end
@@ -206,18 +260,19 @@ function nnn(subs, sel)
 	text=text:gsub(stekst,tekst)
 	
 	    -- GUI for manual breaking
-	    if disable_dialog==false and not do_not_break_1liners and not text:match("\\N") then
+	    if disable_dialog==false and not do_not_break_1liners and not text:match("\\N") or line.effect=="n" then
 		after=text:gsub("^{\\[^}]-}","")
 		dialog=
 		{
 		    {x=0,y=0,width=25,height=4,class="textbox",name="txt",value=after},
-		    {x=0,y=4,width=25,height=1,class="label",label="Use 'Enter' to make linebreaks"},
+		    {x=0,y=4,width=25,class="label",label="Use 'Enter' to make linebreaks"},
 		} 
 		buttons={"OK","All spaces","Cancel"}
 		pressed,res=aegisub.dialog.display(dialog,buttons,{close='Cancel'})
 		if pressed=="OK" then
 		res.txt=res.txt:gsub("\n","\\N") :gsub("\\N "," \\N")
 		text=tags..res.txt
+		if line.effect=="n" then line.effect="" end
 		end
 		if pressed=="All spaces" then res.txt=res.txt:gsub("%s+"," \\N") text=tags..res.txt end
 	    end
@@ -231,7 +286,7 @@ function nnn(subs, sel)
 end
 
 function balance(tekst)
-    if do_balance_checks and tekst:match("\\N") and not tekst:match("\\N%-") and wrd>4 then
+    if balance_checks and tekst:match("\\N") and not tekst:match("\\N%-") and wrd>4 then
 	beforespace=tekst:match("^(.-)\\N")	beforespace=beforespace:gsub("{[^}]-}","")	beforelength=beforespace:len()
 	afterspace=tekst:match("\\N(.-)$")	afterspace=afterspace:gsub("{[^}]-}","")	afterlength=afterspace:len()
 	if beforelength>afterlength then ratio=beforelength/afterlength else ratio=afterlength/beforelength end
@@ -273,6 +328,20 @@ end
 
 function db(t) t=t:gsub("\\N","") return t end
 
+function esc(str)
+str=str:gsub("%%","%%%%")
+str=str:gsub("%(","%%%(")
+str=str:gsub("%)","%%%)")
+str=str:gsub("%[","%%%[")
+str=str:gsub("%]","%%%]")
+str=str:gsub("%.","%%%.")
+str=str:gsub("%*","%%%*")
+str=str:gsub("%-","%%%-")
+str=str:gsub("%+","%%%+")
+str=str:gsub("%?","%%%?")
+return str
+end
+
 function stylechk(subs,stylename)
   for i=1, #subs do
     if subs[i].class=="style" then
@@ -285,8 +354,8 @@ end
 
 function nshift(subs, sel)
     for x, i in ipairs(sel) do
-        local line=subs[i]
-        local text=line.text
+        line=subs[i]
+        text=line.text
 	text=text:gsub("([%a%p])\\N([%a%p])","%1 \\N%2") 
 	    if not text:match("\\N") then text="\\N"..text end
 		text=text:gsub("\\N([^%s{}]+%s?)$","%1")		-- end
@@ -305,8 +374,8 @@ end
 
 function backshift(subs, sel)
     for x, i in ipairs(sel) do
-        local line=subs[i]
-        local text=line.text
+        line=subs[i]
+        text=line.text
 	text=text:gsub("([%a%p])\\N([%a%p])","%1 \\N%2") 
 	    if not text:match("\\N") then text=text.."\\N" end
 		text=text:gsub("^({[^}]-}%s?[^%s{}]+%s?)\\N","%1")	-- start
@@ -324,4 +393,4 @@ end
 
 aegisub.register_macro("Line Breaker/Insert Linebreak", script_description, nnn)
 aegisub.register_macro("Line Breaker/Shift Linebreak", script_description, nshift)
-if enable_shift_backwards then aegisub.register_macro("Line Breaker/Shift Linebreak Back", script_description, backshift) end
+aegisub.register_macro("Line Breaker/Shift Linebreak Back", script_description, backshift)
