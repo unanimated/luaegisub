392b0eb1f257f68db484ad809054593607ce9d1c
unanimated <infernal.eternal@safe-mail.net>
2014-12-08 00:30:21 +0100

    2.7

--- time_signs2.lua
+++ time_signs2.lua
@@ -4,14 +4,15 @@
 script_name="Time signs from timecodes"
 script_description="Rough-times signs from TS timecodes"
 script_author="unanimated"
-script_version="2.61"
+script_version="2.7"
 
-function signtime(subs, sel)
+function signtime(subs,sel)
     for i=#sel,1,-1 do
 	line=subs[sel[i]]
 	text=line.text
 	-- format timecodes
 	text=text
+	:gsub("({.-})({TS.-})","%2%1")
 	:gsub("(%d+):(%d%d):(%d%d)",
 		function(a,b,c) return a*60+b..":"..c end)	-- hours
 	:gsub("^(%d%d%d%d)%s%s*","{TS %1}")			-- ^1234  text
@@ -29,6 +30,7 @@ function signtime(subs, sel)
 	tc=text:match("^{[^}]-}") or ""
 	tc=tc:gsub("(%d+)(%d%d)([^:])","%1:%2%3")
 	text=text:gsub("^{[^}]-}%s*",tc)
+	if res.blur then text=text:gsub("\\blur[%d%.]+",""):gsub("{}",""):gsub("^","{\\blur"..res.bl.."}") end
 	line.text=text
 
 	tstags=text:match("{TS[^}]-}") or ""
@@ -78,6 +80,7 @@ function signtime(subs, sel)
 		end
 	    end
 	    line.text=line.text:gsub("{TS[^}]-}","{TS "..tim.."}")
+	    if res.nots then line.text=line.text:gsub("{TS[^}]-}",""):gsub("{(\\[^}]-)}{(\\[^}]-)}","{%1%2}"):gsub("^({[^}]-})%s*","%1") end
 	    subs.insert(sel[i]+1,line)
 	    if t>1 then table.insert(sel,sel[#sel]+1) end
 	end
@@ -95,29 +98,79 @@ function signtime(subs, sel)
     return sel
 end
 
-function timesigns(subs, sel)
-dialog_config=
-{
-    {x=0,y=0,width=4,class="label",label="Check this if all your timecodes are too late or early:", },
-    {x=0,y=1,width=1,class="checkbox",name="shift",label="Shift timecodes by ",value=false },
-    {x=1,y=1,width=2,class="intedit",name="secs",value=-10,hint="Negative=backward / positive=forward" },
-    {x=3,y=1,width=1,class="label",label=" sec.", },
-    {x=0,y=2,width=4,class="checkbox",name="copy",label="For lines without timecodes, copy from the previous line",value=false},
-    {x=0,y=3,width=2,class="checkbox",name="snap",label="Snapping to keyframes:",value=true },
-    {x=0,y=4,width=2,class="label",label="Frames to search back:", },
-    {x=0,y=5,width=2,class="label",label="Frames to search forward:", },
-    {x=2,y=4,width=2,class="intedit",name="kfs",value="24",step=1,min=1,max=250 },	-- default search back [24]
-    {x=2,y=5,width=2,class="intedit",name="kfe",value="24",step=1,min=1,max=250 },	-- default search forward [24]
+--	Config Stuff	--
+function saveconfig()
+savecfg="Time Signs Settings\n\n"
+  for key,val in ipairs(GUI) do
+    if val.class=="floatedit" or val.class=="intedit" or val.class=="checkbox" and val.name~="save" then
+      savecfg=savecfg..val.name..":"..tf(res[val.name]).."\n"
+    end
+  end
+file=io.open(cfgpath,"w")
+file:write(savecfg)
+file:close()
+ADD({{class="label",label="Config saved to:\n"..cfgpath}},{"OK"},{close='OK'})
+end
+
+function loadconfig()
+cfgpath=aegisub.decode_path("?user").."\\timesigns.conf"
+file=io.open(cfgpath)
+    if file~=nil then
+	konf=file:read("*all")
+	file:close()
+	  for key,val in ipairs(GUI) do
+	    if val.class=="floatedit" or val.class=="checkbox" or val.class=="intedit" then
+	      if konf:match(val.name) then val.value=detf(konf:match(val.name..":(.-)\n")) end
+	    end
+	  end
+    end
+end
+
+function tf(val)
+    if val==true then ret="true"
+    elseif val==false then ret="false"
+    else ret=val end
+    return ret
+end
+
+function detf(txt)
+    if txt=="true" then ret=true
+    elseif txt=="false" then ret=false
+    else ret=txt end
+    return ret
+end
+
+
+function timesigns(subs,sel)
+ADD=aegisub.dialog.display
+GUI={
+    {x=0,y=0,width=4,class="label",label="Check this if all your timecodes are too late or early:"},
+    {x=0,y=1,width=1,class="checkbox",name="shift",label="Shift timecodes by "},
+    {x=1,y=1,width=2,class="floatedit",name="secs",value=-10,hint="Negative=backward / positive=forward",step=0.5},
+    {x=3,y=1,width=1,class="label",label=" sec."},
+    {x=0,y=2,width=4,class="checkbox",name="copy",label="For lines without timecodes, copy from the previous line"},
+    {x=0,y=3,width=4,class="checkbox",name="nots",label="Automatically remove {TS ...} comments"},
+    {x=0,y=4,width=2,class="checkbox",name="blur",label="Automatically add blur:"},
+    {x=2,y=4,width=2,class="floatedit",name="bl",value="0.6"},
+    {x=0,y=5,width=2,class="checkbox",name="snap",label="Snapping to keyframes:",value=true},
+    {x=0,y=6,width=2,class="label",label="Frames to search back:"},
+    {x=0,y=7,width=2,class="label",label="Frames to search forward:"},
+    {x=2,y=6,width=2,class="intedit",name="kfs",value="24",step=1,min=1,max=250},
+    {x=2,y=7,width=2,class="intedit",name="kfe",value="24",step=1,min=1,max=250},
+    {x=0,y=8,width=2,class="checkbox",name="save",label="Save current settings"},
+    {x=2,y=8,width=2,class="label",label="                 [Time Signs v"..script_version.."]"},
 }
+    loadconfig()
     buttons={"No more suffering with SHAFT signs!","Exit"}
-    P,res=aegisub.dialog.display(dialog_config,buttons,{ok='No more suffering with SHAFT signs!',cancel='Exit'})
+    P,res=ADD(GUI,buttons,{ok='No more suffering with SHAFT signs!',cancel='Exit'})
     if P=="Exit" then aegisub.cancel() end
 	ms2fr=aegisub.frame_from_ms
 	fr2ms=aegisub.ms_from_frame
 	keyframes=aegisub.keyframes()
+	if res.save then saveconfig() end
     if P=="No more suffering with SHAFT signs!" then sel=signtime(subs,sel) end
     aegisub.set_undo_point(script_name)
     return sel
 end
 
-aegisub.register_macro(script_name, script_description, timesigns)
+aegisub.register_macro(script_name,script_description,timesigns)
