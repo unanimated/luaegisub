41e997fd3d1e0daf7ca8744dea4186cf19786083
Ghegghe <nicopante98@gmail.com>
2023-07-31 01:55:23 +0200

    feat: add function works on alphas

--- ua.Recalculator.lua
+++ ua.Recalculator.lua
@@ -202,7 +202,15 @@ function multiply(subs,sel)
     if res.clipx or res.clipy then clip=1 else clip=0 end
     for z,i in ipairs(sel) do
 	progress("Processing line: "..z.."/"..#sel)
-        if res.byline then count=z*c linec=z altcount=z*alt else count=c linec=1 altcount=alt end
+	if res.byline then 
+		count=z*c 
+		linec=z 
+		altcount=z*alt 
+	else 
+		count=c 
+		linec=1 
+		altcount=alt 
+	end
 	if not res.alt then altcount=count alt=res.add end
 	line=subs[i]
 	text=line.text
@@ -331,6 +339,31 @@ function multiply(subs,sel)
 	return "\\t("..a..","..calc2(tonumber(b)).."," end) end
 	
 	if res.ly then line.layer=lmcalc(line.layer) end
+	if res.alpha then 
+		text=text:gsub("\\alpha&H([%x]+)&", function(a)
+			return "\\alpha&H" .. hexAdd(a,res.add*linec) .. "&"
+		end)
+	end
+	if res["1a"] then 
+		text=text:gsub("\\1a&H([%x]+)&", function(a)
+			return "\\1a&H" .. hexAdd(a,res.add*linec) .. "&"
+		end)
+	end
+	if res["2a"] then 
+		text=text:gsub("\\2a&H([%x]+)&", function(a)
+			return "\\2a&H" .. hexAdd(a,res.add*linec) .. "&"
+		end)
+	end
+	if res["3a"] then 
+		text=text:gsub("\\3a&H([%x]+)&", function(a)
+			return "\\3a&H" .. hexAdd(a,res.add*linec) .. "&"
+		end)
+	end
+	if res["4a"] then 
+		text=text:gsub("\\4a&H([%x]+)&", function(a)
+			return "\\4a&H" .. hexAdd(a,res.add*linec) .. "&"
+		end)
+	end
 	if res.ml then line.margin_l=lmcalc(line.margin_l) end
 	if res.mr then line.margin_r=lmcalc(line.margin_r) end
 	if res.mv then line.margin_t=lmcalc(line.margin_t) end
@@ -345,6 +378,27 @@ function multiply(subs,sel)
 	line.text=text
         subs[i]=line
     end
+end
+
+function hexAdd(hexString, addValue)
+	if hexString == "FF" and addValue > 0 or 
+		hexString == "00" and addValue < 0  then
+		return hexString
+	elseif #hexString < 3 then
+		local newValue = tonumber(hexString, 16) + addValue
+		if newValue < 0 then
+			newValue = 0
+		elseif newValue > 255 then
+			newValue = 255
+		end
+		newValue = string.format("%X", newValue)
+		if #newValue < 2 then
+			newValue = "0" .. newValue
+		end
+		return newValue
+	else
+		return nil
+	end
 end
 
 function lmcalc(num)
